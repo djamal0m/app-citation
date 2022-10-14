729ff27f1681c98e0400717be94f11c79ed38f29
Djamal Moustapha <43470092+djameuzz@users.noreply.github.com>
2022-10-14 15:01:40 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import { React } from "react";
 import App from './App';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router} from 'react-router-dom';
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
 
-
-const root = ReactDOM.createRoot( document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-      <App/>
-  </Router>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
